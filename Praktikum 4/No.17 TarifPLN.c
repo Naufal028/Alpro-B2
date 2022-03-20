712aa1564c175ea529795418c95d918d73ae5038
Naufal028 <100133075+Naufal028@users.noreply.github.com>
2022-03-20 20:06:43 +0700

    Update No.17 TarifPLN.c

--- Praktikum 4/No.17 TarifPLN.c
+++ Praktikum 4/No.17 TarifPLN.c
@@ -1,5 +1,7 @@
-// Muhammad Naufal - 24060120140157
-// No.17 TarifPLN
+/*Nama File 	: No.17 TarifPLN*/
+/*Deskripsi 	: Menghitung jumlah tarif PLN*/
+/*Pembuat   	: 24060120140157 - Muhammad Naufal*/
+/*Tgl Pembuatan	: 20 Maret 2021*/
 
 #include <stdio.h>
 #include <stdlib.h>
