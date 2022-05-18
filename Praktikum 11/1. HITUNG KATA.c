4b905a63f1683a5fbcb0b2d3b2233f0484c3904d
Naufal028 <100133075+Naufal028@users.noreply.github.com>
2022-05-18 22:42:45 +0700

    Update 1. HITUNG KATA.c

--- Praktikum 11/1. HITUNG KATA.c
+++ Praktikum 11/1. HITUNG KATA.c
@@ -1,3 +1,8 @@
+/*Nama File 	: 1. HITUNG KATA.c*/
+/*Deskripsi 	: Menghitung jumlah karakter pada mesin karakter*/
+/*Pembuat   	: 24060120140157 - Muhammad Naufal*/
+/*Tgl Pembuatan	: 18 Mei 2021*/
+
 #include<stdio.h>
 #include<conio.h>
 #include"mesinint.c"
